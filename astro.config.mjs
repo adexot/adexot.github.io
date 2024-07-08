19762626a864e3dc3021ff2ebeb05337a8c0ac1b
adexot <adekunleoseni47@gmail.com>
2024-07-08 10:32:50 +0200

    chore: update astro version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
   vite: {
     ssr: {
       external: ["svgo"],
